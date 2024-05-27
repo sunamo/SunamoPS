c33188e7dccabe8354671d15c6bc2c12fff7d774
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-27 22:30:42 +0200

    Another files have name as their code element

---
+++ ErrorRecordHelper.cs
@@ -0,0 +1,24 @@
+
+
+namespace SunamoPS;
+
+public class ErrorRecordHelper
+{
+    // ErrorRecord + Exc here
+    // Zjistit proč mi to např u @"C:\repos\_\Bobril_Projects\_tutorials\BobrilYt\" nezobrazuje nikdy nic ač ps konzole vypisuje něco
+
+    public static void Text(StringBuilder sb, ErrorRecord e)
+    {
+        if (e == null)
+        {
+            return; // string.Empty;
+        }
+
+        if (e.ErrorDetails != null)
+        {
+            sb.AppendLine(e.ErrorDetails.Message);
+        }
+
+        sb.AppendLine(Exceptions.TextOfExceptions(e.Exception));
+    }
+}
