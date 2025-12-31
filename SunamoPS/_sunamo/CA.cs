293bbc76371d333d4ccc3a55a4bafc0a62ab4b28
Radek Jancik <radek.jancik@usys.cz>
2025-12-31 10:55:04 +0100

    25.12.31.1 feat: better naming for variables

--- SunamoPS/_sunamo/CA.cs
+++
@@ -1,5 +0,0 @@
-namespace SunamoPS._sunamo;
-
-internal class CA
-{
-}
--- SunamoPS/_sunamo/FS.cs
+++
@@ -1,6 +0,0 @@
-namespace SunamoPS._sunamo;
-
-//namespace SunamoPS._sunamo.SunamoExceptions._AddedToAllCsproj;
-internal class FS
-{
-}
